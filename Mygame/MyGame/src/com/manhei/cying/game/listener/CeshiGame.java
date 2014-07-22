8bc2350b2bb5c3778276deb46480fc103bf4c034
huangcy0314 <huangcy0314@thundersoft.com>
2014-07-22 14:52:23 +0800

    重新修改框架

---
+++ Mygame/MyGame/src/com/manhei/cying/game/listener/CeshiGame.java
@@ -0,0 +1,24 @@
+package com.manhei.cying.game.listener;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+
+public class CeshiGame extends Game {
+    private Screen screen;
+    @Override
+    public void create() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public CeshiGame(Screen screen){
+        super();
+        this.screen = screen;
+    }
+    @Override
+    public void resume() {
+        // TODO Auto-generated method stub
+        super.resume();
+    }
+    
+}
